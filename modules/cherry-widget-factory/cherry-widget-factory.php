22436156ab538aa9affe18ee2d56ca06917adad6
dkfiresky <dkholodilin@live.com>
2016-03-25 16:42:35 +0200

    UPD: update license info

--- modules/cherry-widget-factory/cherry-widget-factory.php
+++ modules/cherry-widget-factory/cherry-widget-factory.php
@@ -5,8 +5,8 @@
  * Version: 1.0.0
  * Author: Cherry Team
  * Author URI: http://www.cherryframework.com/
- * License: GPLv2
- * License URI: http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * License: GPLv3
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.html
  *
  * @package    Cherry_Framework
  * @subpackage Class
@@ -14,7 +14,7 @@
  * @author     Cherry Team <cherryframework@gmail.com>
  * @copyright  Copyright (c) 2012 - 2016, Cherry Team
  * @link       http://www.cherryframework.com/
- * @license    http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * @license    http://www.gnu.org/licenses/gpl-3.0.html
  */
 
 // If this file is called directly, abort.
