3633b11c69b200361eed0cded2ead155ff04f018
gcofficial <gurievcreative@gmail.com>
2016-03-30 15:43:34 +0300

    UPD: travis

--- modules/cherry-creator/cherry-creator.php
+++ modules/cherry-creator/cherry-creator.php
@@ -1,13 +1,6 @@
 <?php
 /**
  * Creator
- *
- * @package    Cherry_Framework
- * @subpackage Class
- * @author     Cherry Team <cherryframework@gmail.com>
- * @copyright  Copyright (c) 2012 - 2016, Cherry Team
- * @link       http://www.cherryframework.com/
- * @license    http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  * Module Name: Creator
  * Description:
  * Version: 1.0.0
