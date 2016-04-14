ad48599c954543a544c8de4957d977cc1bd6c4f3
dkfiresky <dkholodilin@live.com>
2016-04-14 10:23:31 +0300

    UPD: add phpdoc block into file header

--- setup.php
+++ setup.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * @package    Cherry_Framework
+ * @author     Cherry Team <cherryframework@gmail.com>
+ * @copyright  Copyright (c) 2012 - 2016, Cherry Team
+ * @link       http://www.cherryframework.com/
+ * @license    http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ */
 
 return create_function( '', '
 global $__tm_version;
