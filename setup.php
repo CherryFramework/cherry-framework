24680a2e87f888f07a7c68e970c036f9c837cfaf
dkfiresky <dkholodilin@live.com>
2016-04-14 10:56:09 +0300

    UPD: spaces

--- setup.php
+++ setup.php
@@ -12,33 +12,33 @@
  */
 
 return create_function( '', '
-	global $__tm_version;
+    global $__tm_version;
 
-	$path = trailingslashit( __DIR__ ) . \'cherry-core.php\';
+    $path = trailingslashit( __DIR__ ) . \'cherry-core.php\';
 
-	$data = get_file_data( $path, array(
-		\'version\' => \'Version\'
-	) );
+    $data = get_file_data( $path, array(
+        \'version\' => \'Version\'
+    ) );
 
-	if ( isset( $data[\'version\'] ) ) {
-		$version = $data[\'version\'];
-	}
+    if ( isset( $data[\'version\'] ) ) {
+        $version = $data[\'version\'];
+    }
 
-	$old_versions = null;
+    $old_versions = null;
 
-	if ( null !== $__tm_version ) {
-		$old_versions = array_keys( $__tm_version );
-	}
+    if ( null !== $__tm_version ) {
+        $old_versions = array_keys( $__tm_version );
+    }
 
-	if ( is_array( $old_versions ) && isset( $old_versions[0] ) ) {
-		$compare = version_compare( $old_versions[0], $version, \'<\' );
+    if ( is_array( $old_versions ) && isset( $old_versions[0] ) ) {
+        $compare = version_compare( $old_versions[0], $version, \'<\' );
 
-		if ( $compare ) {
-			$__tm_version = array();
-			$__tm_version[ $version ] = $path;
-		}
-	} else {
-		$__tm_version = array();
-		$__tm_version[ $version ] = $path;
-	}
+        if ( $compare ) {
+            $__tm_version = array();
+            $__tm_version[ $version ] = $path;
+        }
+    } else {
+        $__tm_version = array();
+        $__tm_version[ $version ] = $path;
+    }
 ' );
