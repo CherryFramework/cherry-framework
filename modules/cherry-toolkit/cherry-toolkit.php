c3915d8954016a283ba8988e560b443f37e25258
gcofficial <gurievcreative@gmail.com>
2016-04-18 12:22:48 +0300

    DEL: cherry-array_utilit

--- modules/cherry-toolkit/cherry-toolkit.php
+++ modules/cherry-toolkit/cherry-toolkit.php
@@ -131,5 +131,81 @@ if ( ! class_exists( 'Cherry_Toolkit' ) ) {
 			// Return the compiled view and terminate the output buffer.
 			return $result;
 		}
+
+		/**
+		 * Remove empty elements
+		 *
+		 * @param  array $arr --- array with empty elements.
+		 * @return array --- array without empty elements
+		 */
+		public static function remove_empty( $arr ) {
+			return array_filter( $arr, array( __CLASS__, 'remove_empty_check' ) );
+		}
+
+		/**
+		 * Check if empty.
+		 * It's need for PHP 5.2.4 version
+		 *
+		 * @param  [type] $var variable.
+		 * @return boolean
+		 */
+		public static function remove_empty_check( $var ) {
+			return '' != $var;
+		}
+
+		/**
+		 * Join array to string
+		 *
+		 * @param  array $arr --- array like 'key' => 'value'.
+		 * @return string --- joined string
+		 */
+		public static function join( $arr = array() ) {
+			$arr    = self::remove_empty( $arr );
+			$result = array();
+			foreach ( $arr as $key => $value ) {
+				$result[] = sprintf( '%s="%s"', $key, $value );
+			}
+			return implode( ' ', $result );
+		}
+
+		/**
+		 * Lave just right keys in array
+		 *
+		 * @param  array $right_keys right keys to leave.
+		 * @param  array $array list.
+		 * @return array
+		 */
+		public static function leave_right_keys( $right_keys, $array ) {
+			$right_keys = (array) $right_keys;
+			$array      = (array) $array;
+			if ( count( $array ) ) {
+				foreach ( $array as $key => $value ) {
+					if ( ! in_array( $key, $right_keys ) ) {
+						unset( $array[ $key ] );
+					}
+				}
+			}
+			return $array;
+		}
+
+		/**
+		 * Remove some keys form array
+		 *
+		 * @param  [type] $right_keys keys to remove.
+		 * @param  [type] $array      where we want remove this keys.
+		 * @return array without keys
+		 */
+		public static function remove_right_keys( $right_keys, $array ) {
+			$right_keys = (array) $right_keys;
+			$array      = (array) $array;
+			if ( count( $right_keys ) ) {
+				foreach ( $right_keys as $key ) {
+					if ( array_key_exists( $key, $array ) ) {
+						unset( $array[ $key ] );
+					}
+				}
+			}
+			return $array;
+		}
 	}
 }
