f7025917f12bc95b7cc35f5c9d3d1608a3827e00
shin <shin@templatemonster.me>
2016-05-27 16:20:08 +0300

    upd: ui redesign

--- modules/cherry-ui-elements/inc/ui-elements/ui-switcher/ui-switcher.php
+++ modules/cherry-ui-elements/inc/ui-elements/ui-switcher/ui-switcher.php
@@ -74,6 +74,7 @@ if ( ! class_exists( 'UI_Switcher' ) ) {
 				$html .= '<div class="cherry-switcher-wrap size-' . esc_attr( $this->settings['style'] ) . ' ' . esc_attr( $this->settings['class'] ) . '">';
 					$html .= '<label class="sw-enable"><span>' . esc_html( $this->settings['toggle']['true_toggle'] ) . '</span></label>';
 					$html .= '<label class="sw-disable"><span>' . esc_html( $this->settings['toggle']['false_toggle'] ) . '</span></label>';
+					$html .= '<span class="state-marker"></span>';
 					$html .= '<input id="' . esc_attr( $this->settings['id'] ) . '" type="hidden" class="cherry-input-switcher" name="' . esc_attr( $this->settings['name'] ) . '" ' . checked( 'true', $this->settings['value'], false ) . ' value="' . esc_html( $this->settings['value'] ) . '" ' . $data_attr_line . '>';
 				$html .= '</div>';
 			$html .= '</div>';
