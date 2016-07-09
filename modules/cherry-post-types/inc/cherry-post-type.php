a306e213f93591cd379246ccac4be6a0dfc56645
cheh <chehovskiy.dima@gmail.com>
2016-07-09 15:10:41 +0300

    UPD: labels params for post type registration

--- modules/cherry-post-types/inc/cherry-post-type.php
+++ modules/cherry-post-types/inc/cherry-post-type.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Custom post type
+ * Custom post type.
  *
  * @package    Cherry_Framework
  * @subpackage Class
@@ -11,57 +11,56 @@
  */
 
 /**
- * Cherry_Post_Type class
+ * Cherry_Post_Type class.
+ *
+ * @since 1.0.0
  */
 class Cherry_Post_Type {
 
 	/**
-	 * Post type slug
+	 * Post type slug.
 	 *
-	 * @var null
+	 * @since 1.0.0
+	 * @var string
 	 */
-	private $slug = null;
+	private $slug = '';
 
 	/**
-	 * Post type arguments
+	 * Post type arguments.
 	 *
-	 * @var null
+	 * @since 1.0.0
+	 * @var array
 	 */
-	private $args = null;
+	private $args = array();
 
 	/**
 	 * The registered custom post type.
 	 *
+	 * @since 1.0.0
 	 * @var Object|\WP_Error
 	 */
 	private $post_type;
 
 	/**
-	 * Font awesome icon name.
+	 * Constructor.
 	 *
-	 * @var null
-	 */
-	private $icon = null;
-
-	/**
-	 * Cherry Post Type Builder class constructor
-	 *
-	 * @param [type] $slug post type slug.
-	 * @param [type] $args post type arguments.
+	 * @since 1.0.0
+	 * @param sring $slug Post type slug.
+	 * @param array $args Post type arguments.
 	 */
 	public function __construct( $slug, $args ) {
-
 		$this->slug = $slug;
 		$this->args = $args;
 
 		// Register post type
-		add_action( 'init', array( &$this, 'register' ) );
+		add_action( 'init', array( $this, 'register' ) );
 	}
 
 	/**
 	 * Triggered by the 'init' action event.
 	 * Register a WordPress custom post type.
 	 *
+	 * @since 1.0.0
 	 * @return void
 	 */
 	public function register() {
@@ -69,44 +68,5 @@ class Cherry_Post_Type {
 			$this->slug,
 			$this->args
 		);
-	}
-
-	/**
-	 * Add font awesome icon to menu
-	 *
-	 * @param  [type] $icon font awesome icon code.
-	 * @return boolen true if succes | false if not.
-	 */
-	public function font_awesome_icon( $icon = '' ) {
-		if ( '' === $icon ) {
-			return false;
-		}
-
-		$this->icon = $icon;
-
-		add_action( 'admin_enqueue_scripts', array( &$this, 'load_font_awesome' ) );
-
-		return true;
-	}
-
-	/**
-	 * Load font awesome fonts to admin menu.
-	 *
-	 * @return void
-	 */
-	public function load_font_awesome() {
-		wp_enqueue_style(
-			'font-awesome',
-			'//maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css'
-		);
-
-		?>
-		<style type='text/css' media='screen'>
-			#adminmenu .menu-icon-<?php echo $this->slug; ?> div.wp-menu-image:before {
-				font-family: Fontawesome !important;
-				content: '\<?php echo $this->icon; ?>';
-			}
-		</style>
-		<?php
 	}
 }
