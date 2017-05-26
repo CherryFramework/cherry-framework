1beb990b29139ece0bdef127605f08bc5a71a683
Makhonko Denis <makhonkodenis@gmail.com>
2017-05-26 18:11:20 +0300

    FIX: placeholder in ui select

--- modules/cherry-ui-elements/inc/ui-elements/ui-select/ui-select.php
+++ modules/cherry-ui-elements/inc/ui-elements/ui-select/ui-select.php
@@ -111,7 +111,7 @@ if ( ! class_exists( 'UI_Select' ) ) {
 
 				$inline_style = $this->settings['inline_style'] ? 'style="' . esc_attr( $this->settings['inline_style'] ) . '"' : '' ;
 
-				$html .= '<select id="' . esc_attr( $this->settings['id'] ) . '" class="cherry-ui-select" name="' . esc_attr( $name ) . '" size="' . esc_attr( $this->settings['size'] ) . '" ' . $multi_state . ' ' . $filter_state . ' placeholder="' . $this->settings['placeholder'] . '" ' . $inline_style . ' ' . $input_lock . '>';
+				$html .= '<select id="' . esc_attr( $this->settings['id'] ) . '" class="cherry-ui-select" name="' . esc_attr( $name ) . '" size="' . esc_attr( $this->settings['size'] ) . '" ' . $multi_state . ' ' . $filter_state . ' data-placeholder="' . esc_attr( $this->settings['placeholder'] ) . '" ' . $inline_style . ' ' . $input_lock . '>';
 
 				if ( $this->settings['options'] && ! empty( $this->settings['options'] ) && is_array( $this->settings['options'] ) ) {
 					foreach ( $this->settings['options'] as $option => $option_value ) {
