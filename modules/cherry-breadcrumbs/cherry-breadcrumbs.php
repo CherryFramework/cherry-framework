1bffa93549db26600982601695ef78d0a04e90c9
mjhead <andremjhead@gmail.com>
2017-06-30 17:51:16 +0300

    FIX: breadcrumbs search behavior

--- modules/cherry-breadcrumbs/cherry-breadcrumbs.php
+++ modules/cherry-breadcrumbs/cherry-breadcrumbs.php
@@ -1255,10 +1255,9 @@ if ( ! class_exists( 'Cherry_Breadcrumbs' ) ) {
 			if ( is_paged() ) {
 				$url   = get_search_link();
 				$this->_add_item( 'link_format', $label, $url );
-
+			} else {
+				$this->_add_item( 'target_format', $label );
 			}
-
-			$this->_add_item( 'target_format', $label );
 		}
 
 		/**
